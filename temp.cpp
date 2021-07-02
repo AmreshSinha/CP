661caecd11ffc498edb0ac0f331dae9e8c893b4b
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2021-07-02 13:23:07 +0530

    CP Template First Commit

---
+++ temp.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Amresh on 02-07-2021.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // Solution come here
+
+}
